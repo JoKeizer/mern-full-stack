09a1a0757897df2ee3fc418968ea65bdf4b5ebf0
Jo Keizer <jolanda@sociossports.nl>
2020-04-16 16:00:29 +0200

    Add Comments and delete comments

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -63,7 +63,7 @@ export const removeLike = id => async dispatch => {
 };
 
 // Delete post
-export const deltePost = id => async dispatch => {
+export const deletePost = id => async dispatch => {
   try {
     const res = await axios.delete(`/api/posts/${id}`);
 
@@ -99,6 +99,70 @@ export const addPost = formData => async dispatch => {
       payload: res.data
     });
     dispatch(setAlert('Post Created', 'success'));
+
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+
+// Add Comment
+export const addComment = (postId, formData) => async dispatch => {
+
+  //We want to send data!!!
+  const config = {
+    'Content-Type': 'application/json'
+  }
+
+  try {
+    //send formdata and config
+    const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+    dispatch(setAlert('Comment Added', 'success'));
+
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
 
   } catch (err) {
     dispatch({
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,8 +3,10 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     DELETE_POST,
-    ADD_POST
-
+    ADD_POST,
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 }from '../actions/types'
 
 const initialState = {
@@ -25,11 +27,17 @@ export default function(state = initialState, action) {
                 loading: false,
             }
         case ADD_POST: 
-        return {
-            ...state,
-            posts: [payload, ...state.posts],
-            loading: false
-        }
+            return {
+                ...state,
+                posts: [payload, ...state.posts],
+                loading: false
+            }
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading: false,
+            }
         case POST_ERROR:
             return {
                 ...state,
@@ -50,6 +58,21 @@ export default function(state = initialState, action) {
                     posts: state.posts.filter(post => post._id !== payload),
                     loading: false
                 };
+            case ADD_COMMENT:
+                return {
+                    ...state,
+                    post: {...state.post, comments: payload},
+                    loading: false
+                }
+            case REMOVE_COMMENT:
+                return {
+                    ...state,
+                    post: {
+                        ...state.post, 
+                        comments: state.post.comments.filter(comment => comment._id !== payload)},
+                    loading: false
+
+                }
         default:
             return state
     }
